fbddeabae79fb6cc2274a82611770da03ac485a2
Taiki Yamada <t.yamada@chapter-5.com>
2020-04-06 17:06:33 +0900

    fix test bug

--- handlers/db/testhelper.go
+++ handlers/db/testhelper.go
@@ -1,17 +1,14 @@
 package db
 
 import (
-	"fmt"
-
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/guregu/dynamo"
-	"github.com/pkg/errors"
 )
 
 var (
-	Region        = "ap-northeast-1"
-	Endpoint      = "http://localhost:8000"
+	Endpoint = "http://localhost:8000"
 )
 
 type CreateTable struct {
@@ -23,14 +20,13 @@ type CreateTable struct {
 	localIndices  map[string]dynamodb.LocalSecondaryIndex
 	readUnits     int64
 	writeUnits    int64
-	streamView    StreamView
 	ondemand      bool
 	tags          []*dynamodb.Tag
 	err           error
 }
 
 func TestNew() DB {
-	db := dynamo.New(session.New(), &aws.Config{Region: aws.String(Region), Endpoint: aws.String(Endpoint)}),
+	db := dynamo.New(session.New(), &aws.Config{Region: aws.String(Region), Endpoint: aws.String(Endpoint)})
 
 	return DB{Instance: db}
 }
@@ -52,27 +48,28 @@ func (d DB) DeleteLinkTable() error {
 }
 
 func (d DB) LinkTableExists() (bool, error) {
-	output, err := d.Instance.ListTables()
+	output, err := d.Instance.ListTables().All()
 	if err != nil {
-		return false, errors.Wrap(err, "failed to list tables")
+		return false, err
 	}
-	if contains(output.TableNames, LinkTableName) {
+
+	if contains(output, LinkTableName) {
 		return true, nil
 	}
 
 	return false, nil
 }
 
-func contains(s []*string, e string) bool {
+func contains(s []string, e string) bool {
 	for _, a := range s {
-		if a == nil {
+		if a == "" {
 			continue
 		}
 
-		if *a == e {
+		if a == e {
 			return true
 		}
-
-		return false
 	}
+
+	return false
 }
--- handlers/user/create/main_test.go
+++ handlers/user/create/main_test.go
@@ -6,9 +6,9 @@ import (
 	"os"
 	"testing"
 
-	"github.com/pkg/errors"
-	"github.com/aws/aws-lambda-go/events"
 	"github.com/Tai-Yaki/lambda-practice-go/handler/db"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/pkg/errors"
 )
 
 const exitError = 1
@@ -16,8 +16,8 @@ const exitError = 1
 func TestHandler(t *testing.T) {
 	tests := []struct {
 		name, email, password, method string
-		status      int
-	} {
+		status                        int
+	}{
 		{"test", "test@example.com", "aaaa1234", http.MethodPost, http.StatusOK},
 		{"test", "test@example.com", "", http.MethodPost, http.StatusBadRequest},
 		{"test", "test@example.com", "aaaa1234", http.MethodGet, http.StatusBadRequest},
@@ -26,9 +26,9 @@ func TestHandler(t *testing.T) {
 	for _, te := range tests {
 		res, _ := handler(events.APIGatewayProxyRequest{
 			HTTPMethod: te.method,
-			Body:       "{\"name\": \"" + te.name +
-						"\", \"email\": \"" + te.email +
-						", \"password\": \"" + te.password + "\"}",
+			Body: "{\"name\": \"" + te.name +
+				"\", \"email\": \"" + te.email +
+				", \"password\": \"" + te.password + "\"}",
 		})
 
 		if res.StatusCode != te.status {
@@ -71,7 +71,8 @@ func prepare() error {
 }
 
 func cleanUp() error {
-	ok, err != nil {
+	ok, err := DynamoDB.LinkTableExists()
+	if err != nil {
 		return errors.Wrap(err, "failed to check table existence")
 	}
 	if ok {
